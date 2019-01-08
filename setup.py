305a6211c17ac0b7fc93a5c8fd7e84537880a745
Alex Metzger <asm@asm.io>
2019-01-08 16:15:38 -0500

    Support for Bitbucket Server downloading

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-
 import setuptools
 
 with open("README.md", "r") as fh:
@@ -6,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jf_agent",
-    version="0.0.3",
+    version="0.0.4",
     description="An agent for collecting data for jellyfish",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -15,14 +14,14 @@ setuptools.setup(
     install_requires=[
         'jira',
         'tqdm',
+        'stashy',
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    entry_points = {
+    entry_points={
         'console_scripts': ['jf_agent=jf_agent.main:main'],
-    }    
+    }
 )
-
