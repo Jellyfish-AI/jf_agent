474fd2e8ae572363eb1576f686a1a9f7a9fbfdc2
Matt Klein <matt@jelly.ai>
2019-03-08 16:32:03 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jf_agent",
-    version="0.0.7",
+    version="0.0.8",
     description="An agent for collecting data for jellyfish",
     long_description=long_description,
     long_description_content_type="text/markdown",
