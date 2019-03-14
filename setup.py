8e957dc9ea6f92fb6c1fdc41db431430d08d3592
Eli Daniel <eli@jellyfish.co>
2019-03-14 09:43:31 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jf_agent",
-    version="0.0.8",
+    version="0.0.9",
     description="An agent for collecting data for jellyfish",
     long_description=long_description,
     long_description_content_type="text/markdown",
