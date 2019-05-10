6b02a1f22f072d377d7e0839ef2da4e1c2ec9090
Eli Daniel <eliwind@users.noreply.github.com>
2019-05-09 21:01:21 -0400

    Allow pulling git data in windows instead of all time (#11)

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jf_agent",
-    version="0.0.14",
+    version="0.0.15",
     description="An agent for collecting data for jellyfish",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Jellyfish-AI/jf_agent",
     packages=setuptools.find_packages(),
-    install_requires=["jira", "tqdm", "stashy", "pyyaml>=4.2b1", "urllib3==1.24.2"],
+    install_requires=["jira", "tqdm", "stashy", "pyyaml>=4.2b1", "urllib3==1.24.2", "dateparser"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
