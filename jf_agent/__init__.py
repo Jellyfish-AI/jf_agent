7f5f7f0853c9584519e87437dcc302fbe1cc944a
Matt Klein <matt@jelly.ai>
2019-09-24 12:11:29 -0400

    Use streaming JSON to write commits and PRs in addition to Jira issues; factor this logic into new method download_and_write_streaming; factor out write_file so that it's in __init__.py alongside download_and_write_streaming

--- jf_agent/__init__.py
+++ jf_agent/__init__.py
@@ -1,4 +1,7 @@
 from datetime import datetime, timedelta
+import gzip
+import json
+import jsonstreams
 import pytz
 
 
@@ -35,3 +38,41 @@ def pull_since_date_for_repo(server_git_instance_info, org_login, repo_id, commi
     else:
         # We need to backpopulate the repo
         return instance_pull_from_dt
+
+
+def write_file(outdir, filename_prefix, compress, results):
+    if compress:
+        with gzip.open(f'{outdir}/{filename_prefix}.json.gz', 'wb') as outfile:
+            outfile.write(json.dumps(results, indent=2, default=str).encode('utf-8'))
+    else:
+        with open(f'{outdir}/{filename_prefix}.json', 'w') as outfile:
+            outfile.write(json.dumps(results, indent=2, default=str))
+
+
+class StrDefaultEncoder(json.JSONEncoder):
+    def __init__(self, *args, **kwargs):
+        kwargs.update({'default': str})
+        super().__init__(*args, **kwargs)
+
+
+def download_and_write_streaming(
+    outdir, filename_prefix, compress, generator_func, generator_func_args, item_id_dict_key
+):
+    if compress:
+        outfile = gzip.open(f'{outdir}/{filename_prefix}.json.gz', 'wt')
+    else:
+        outfile = open(f'{outdir}/{filename_prefix}.json', 'w')
+
+    item_ids = set()
+    with jsonstreams.Stream(jsonstreams.Type.array, fd=outfile, encoder=StrDefaultEncoder) as s:
+        for item in generator_func(*generator_func_args):
+            if isinstance(item, list):
+                for i in item:
+                    s.write(i)
+                    item_ids.add(i[item_id_dict_key])
+            else:
+                s.write(item)
+                item_ids.add(item[item_id_dict_key])
+
+    outfile.close()
+    return item_ids
