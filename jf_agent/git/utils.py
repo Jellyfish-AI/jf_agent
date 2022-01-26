1dc0c3c3a8863281f6a757163c923229a29fd5a4
lukasbrower <lukas@jellyfish.co>
2022-01-26 11:33:49 -0500

    Switch to use fnmatch instead of regex to limit use to just wildcards

--- jf_agent/git/utils.py
+++ jf_agent/git/utils.py
@@ -1,4 +1,4 @@
-import re
+import fnmatch
 from typing import List
 from jf_agent.git import NormalizedRepository
 
@@ -12,11 +12,12 @@ def get_branches_for_normalized_repo(repo: NormalizedRepository, included_branch
         branches_to_process.extend(get_matching_branches(additional_branches_for_repo, repo_branch_names))
     return set(branches_to_process)
 
-# Given a list of patterns, either literal branch names or regexes meant to match a set of branches in a repo, 
-# return the list of branches from repo_branches that match any of the branch name patterns.
+# Given a list of patterns, either literal branch names or names with wildcards (*) meant to match a set of branches in a repo, 
+# return the list of branches from repo_branches that match any of the branch name patterns. 
+# fnmatch is used over regex to support wildcards but avoid complicating the requirements on branch naming in a user's config.
 def get_matching_branches(included_branch_patterns: List[str], repo_branch_names: List[str]) -> List[str]:
     matching_branches = []
     for repo_branch_name in repo_branch_names:
-        if any(re.fullmatch(pattern, repo_branch_name) for pattern in included_branch_patterns):
+        if (any(fnmatch.fnmatch(repo_branch_name, pattern) for pattern in included_branch_patterns)):
             matching_branches.append(repo_branch_name)
     return matching_branches
