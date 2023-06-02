29b3a8be00239f5d68ff33ce163b6c326b7471c7
Gavin Pitt <103140978+gavinpitt-jf@users.noreply.github.com>
2023-06-02 15:22:34 -0400

    OJ-25733: Adding projet key information to agent jira manifests (#224)

--- jf_agent/data_manifests/jira/adapter.py
+++ jf_agent/data_manifests/jira/adapter.py
@@ -1,13 +1,11 @@
+import json
+
 from dataclasses import dataclass
 from functools import partial
-import logging
 from typing import Callable, Generator
 from jf_agent.jf_jira import get_basic_jira_connection
 from jf_agent.jf_jira.jira_download import download_users
 from jira import JIRAError
-
-
-logger = logging.getLogger(__name__)
 
 
 @dataclass
@@ -15,20 +13,7 @@ class JiraCloudManifestAdapter:
     def __init__(self, config, creds) -> None:
         self.jira_connection = get_basic_jira_connection(config, creds)
         self.config = config
-
-    def _get_all_pages(self, url_path: str, page_size: int = 50) -> list:
-        curr = 0
-        all_items = []
-        while True:
-            page = self.jira_connection._get_json(
-                url_path, {'startAt': curr, "maxResults": page_size}
-            )
-            all_items += page
-            curr += page_size
-
-            if len(page) == 0:
-                break
-        return all_items
+        self.jira_url = config.jira_url
 
     def _get_jql_search(self, jql_search: str, max_results: int = 0):
         return self.jira_connection._get_json(
@@ -83,23 +68,44 @@ class JiraCloudManifestAdapter:
     def get_priorities_count(self) -> int:
         return len(self.jira_connection.priorities())
 
+    def _get_projects(self) -> Generator[dict, None, None]:
+        for project in self._page_get_results(
+            url=f'{self.jira_url}/rest/api/latest/project/search?startAt=%s&maxResults=500&status=archived&status=live'
+        ):
+            yield project
+
     def get_projects_count(self) -> int:
-        return len(self.jira_connection.projects())
+        url = f'{self.jira_url}/rest/api/latest/project/search?startAt=0&maxResults=0&status=archived&status=live'
+        result = self._get_raw_result(url=url)
+        return result['total']
+
+    def get_project_keys(self) -> list[str]:
+        return [project['key'] for project in self._get_projects()]
 
     def get_project_versions_count(self) -> int:
-        return sum(
-            [len(self.jira_connection.project_versions(p)) for p in self.jira_connection.projects()]
-        )
+        total_project_versions = 0
+        for project in self._get_projects():
+            total_project_versions += self._get_raw_result(
+                url=f'{self.jira_url}/rest/api/latest/project/{project["id"]}/version?startAt=0&maxResults=0&state=future,active,closed'
+            )['total']
+
+        return total_project_versions
 
     def get_boards_count(self) -> int:
         return len([b for b in self._func_paginator(func_endpoint=self.jira_connection.boards)])
 
     def get_sprints_count(self) -> int:
-        total_sprints = 0
-        for board in self._func_paginator(func_endpoint=self.jira_connection.boards):
-            bound_sprints_func = partial(self.jira_connection.sprints, board.id)
+        def _get_sprints_for_board(board_id: int):
+            total_sprints_for_board = 0
             try:
-                total_sprints += len([s for s in self._func_paginator(bound_sprints_func)])
+                total_sprints_for_board += len(
+                    [
+                        sprint
+                        for sprint in self._page_get_results(
+                            url=f'{self.jira_url}/rest/agile/1.0/board/{board_id}/sprint?startAt=%s&maxResults=100'
+                        )
+                    ]
+                )
             except JIRAError as e:
                 # From what I can tell, there isn't an easy way to tell
                 # from the 'board' object if sprints are enabled or not
@@ -107,15 +113,24 @@ class JiraCloudManifestAdapter:
                     e.text == 'The board does not support sprints'
                     or e.text == 'The board doesn\'t support sprints.'
                 ):
-                    continue
+                    print(f"Couldn't get sprints for board {board_id}")
                 elif (
                     e.status_code == 500
                     and e.text == 'This board has no columns with a mapped status.'
                 ):
-                    continue
+                    print(f"Board {board_id} doesn't support sprints -- skipping")
                 else:
                     raise
-        return total_sprints
+
+            return total_sprints_for_board
+
+        total_sprints_across_all_boards = 0
+        for board in self._page_get_results(
+            url=f'{self.jira_url}/rest/agile/1.0/board?startAt=%s&maxResults=100'
+        ):
+            total_sprints_across_all_boards += _get_sprints_for_board(board["id"])
+
+        return total_sprints_across_all_boards
 
     def get_issues_count(self) -> int:
         # Query for all issues via JQL, but ask for 0 results.
@@ -126,3 +141,21 @@ class JiraCloudManifestAdapter:
 
     def get_issues_data_count(self) -> int:
         return self.get_issues_count()
+
+    def _get_raw_result(self, url) -> dict:
+        response = self.jira_connection._session.get(url)
+        response.raise_for_status()
+        json_str = response.content.decode()
+        return json.loads(json_str)
+
+    def _page_get_results(self, url: str):
+        start_at = 0
+        while True:
+            page_result = self._get_raw_result(url % start_at)
+            for value in page_result['values']:
+                yield value
+
+            if page_result['isLast']:
+                break
+            else:
+                start_at += len(page_result['values'])
