aad4dfa43c6e12e1bb755242535ebfcc9174f3c9
Kirk Hardy <kirk.hardy@jellyfish.co>
2023-07-12 20:31:09 -0400

    scminfo is not actually a sensitive string but we'll get rid of it to make secrets checker happy

--- tests/test_jira_download.py
+++ tests/test_jira_download.py
@@ -14,7 +14,7 @@ def get_connection():
     mock_server_info_resp = '{"baseUrl":"https://test-co.atlassian.net","version":"1001.0.0-SNAPSHOT",' \
                             '"versionNumbers":[1001,0,0],"deploymentType":"Cloud","buildNumber":100218,' \
                             '"buildDate":"2023-03-16T08:21:48.000-0400","serverTime":"2023-03-17T16:32:45.255-0400",' \
-                            '"scmInfo":"9826a048a245f608f01466bdff7c83ae3899b12d","serverTitle":"JIRA",' \
+                            '"scmInfo":"9999999999999999999999999999999999999999","serverTitle":"JIRA",' \
                             '"defaultLocale":{"locale":"en_US"}} '
     mock_field_resp = '[{"id":"statuscategorychangedate","key":"statuscategorychangedate","name":"Status Category ' \
                       'Changed","custom":false,"orderable":false,"navigable":true,"searchable":true,"clauseNames":[' \
