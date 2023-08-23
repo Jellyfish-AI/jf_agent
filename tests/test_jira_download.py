add152b975fb22c20ed38e18cae0546383088314
Gavin Pitt <gavin@jellyfish.co>
2023-08-23 15:08:29 -0400

    OJ-28807: install test dependencies and update test

--- tests/test_jira_download.py
+++ tests/test_jira_download.py
@@ -9,21 +9,25 @@ from jf_agent.jf_jira.jira_download import get_issues, download_all_issue_metada
 
 
 def get_connection():
-    mock_server_info_resp = '{"baseUrl":"https://test-co.atlassian.net","version":"1001.0.0-SNAPSHOT",' \
-                            '"versionNumbers":[1001,0,0],"deploymentType":"Cloud","buildNumber":100218,' \
-                            '"buildDate":"2023-03-16T08:21:48.000-0400","serverTime":"2023-03-17T16:32:45.255-0400",' \
-                            '"scmInfo":"9999999999999999999999999999999999999999","serverTitle":"JIRA",' \
-                            '"defaultLocale":{"locale":"en_US"}} '
-    mock_field_resp = '[{"id":"statuscategorychangedate","key":"statuscategorychangedate","name":"Status Category ' \
-                      'Changed","custom":false,"orderable":false,"navigable":true,"searchable":true,"clauseNames":[' \
-                      '"statusCategoryChangedDate"],"schema":{"type":"datetime",' \
-                      '"system":"statuscategorychangedate"}}] '
+    mock_server_info_resp = (
+        '{"baseUrl":"https://test-co.atlassian.net","version":"1001.0.0-SNAPSHOT",'
+        '"versionNumbers":[1001,0,0],"deploymentType":"Cloud","buildNumber":100218,'
+        '"buildDate":"2023-03-16T08:21:48.000-0400","serverTime":"2023-03-17T16:32:45.255-0400",'
+        '"scmInfo":"9999999999999999999999999999999999999999","serverTitle":"JIRA",'
+        '"defaultLocale":{"locale":"en_US"}} '
+    )
+    mock_field_resp = (
+        '[{"id":"statuscategorychangedate","key":"statuscategorychangedate","name":"Status Category '
+        'Changed","custom":false,"orderable":false,"navigable":true,"searchable":true,"clauseNames":['
+        '"statusCategoryChangedDate"],"schema":{"type":"datetime",'
+        '"system":"statuscategorychangedate"}}] '
+    )
 
-    class PartialConfig():
+    class PartialConfig:
         jira_url = 'https://test-co.atlassian.net/'
         skip_ssl_verification = False
 
-    class PartialCreds():
+    class PartialCreds:
         jira_password = None
         jira_username = None
         jira_bearer_token = 'asdf'
@@ -36,8 +40,14 @@ def get_connection():
 
     # https://test-co.atlassian.net/rest/api/2/serverInfo
     with requests_mock.Mocker() as m:
-        m.register_uri('GET', 'https://test-co.atlassian.net/rest/api/2/serverInfo', text=f'{mock_server_info_resp}')
-        m.register_uri('GET', 'https://test-co.atlassian.net/rest/api/2/field', text=f'{mock_field_resp}')
+        m.register_uri(
+            'GET',
+            'https://test-co.atlassian.net/rest/api/2/serverInfo',
+            text=f'{mock_server_info_resp}',
+        )
+        m.register_uri(
+            'GET', 'https://test-co.atlassian.net/rest/api/2/field', text=f'{mock_field_resp}'
+        )
         jira_conn = get_basic_jira_connection(config, creds)
 
     return jira_conn
@@ -45,7 +55,9 @@ def get_connection():
 
 class TestJiraDownload(TestCase):
     URL_BASE = 'https://test-co.atlassian.net/rest/api/2/search'
-    URL_TEMPLATE_START = '?jql=project+in+%28%22TC%22%29+and+updatedDate+%3E+0+order+by+id+asc&startAt='
+    URL_TEMPLATE_START = (
+        '?jql=project+in+%28%22TC%22%29+and+updatedDate+%3E+0+order+by+id+asc&startAt='
+    )
     URL_TEMPLATE_END = '&validateQuery=True&fields=updated&maxResults='
     ISSUE_JQL = 'project in ("TC") and updatedDate > 0'
     HTTP_500_JSON = '{"errorMessages":["Internal server error"],"errors":{}}'
@@ -65,9 +77,7 @@ class TestJiraDownload(TestCase):
             "id": "9999",
             "self": "https://test-co.atlassian.net/rest/api/2/issue/9999",
             "key": "TC-BAD",
-            "fields": {
-                "updated": "2022-12-08T18:43:33.703-0500"
-            }
+            "fields": {"updated": "2022-12-08T18:43:33.703-0500"},
         }
         json_response = json.loads(self.mock_response)
         json_response['issues'].insert(50, bad_issue_json)
@@ -90,18 +100,24 @@ class TestJiraDownload(TestCase):
     @classmethod
     def setUpClass(cls):
         cls.jira_connection = get_connection()
-        with open("test_data/jira/test_issues_response.json", "r") as issues_file:
+        with open("tests/test_data/jira/test_issues_response.json", "r") as issues_file:
             issue_json = issues_file.read()
         cls.mock_response = issue_json
 
     def test_get_issues_once(self):
         with requests_mock.Mocker() as m:
-            m.register_uri('GET',
-                           f'{self.URL_BASE}{self.URL_TEMPLATE_START}'
-                           f'{self.start_at}{self.URL_TEMPLATE_END}{self.max_results}',
-                           text=self.mock_response)
-            time.sleep(0.5)  # sometimes there's a split second before the registration is fully in effect
-            issues = get_issues(self.jira_connection, issue_jql=self.ISSUE_JQL, start_at=0, batch_size=100)
+            m.register_uri(
+                'GET',
+                f'{self.URL_BASE}{self.URL_TEMPLATE_START}'
+                f'{self.start_at}{self.URL_TEMPLATE_END}{self.max_results}',
+                text=self.mock_response,
+            )
+            time.sleep(
+                0.5
+            )  # sometimes there's a split second before the registration is fully in effect
+            issues = get_issues(
+                self.jira_connection, issue_jql=self.ISSUE_JQL, start_at=0, batch_size=100
+            )
 
         self.assertEqual(len(issues), 100)
         self.assertTrue(all(isinstance(issue, JiraIssue) for issue in issues))
@@ -117,6 +133,11 @@ class TestJiraDownload(TestCase):
             # batch size starts at 1000 and gets halved until 0
             m.register_uri('GET', f'{self.URL_BASE}', text=self.one_bad_issue_callback)
             issue_metadata = download_all_issue_metadata(
-                self.jira_connection, project_ids, earliest_issue_dt, num_parallel_threads, issue_filter)
+                self.jira_connection,
+                project_ids,
+                earliest_issue_dt,
+                num_parallel_threads,
+                issue_filter,
+            )
 
         self.assertGreaterEqual(len(issue_metadata), 1)
