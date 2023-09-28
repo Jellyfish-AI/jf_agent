49d861f4cf9b13f3baa4cdbd27935f1a05d948ef
Kirk Hardy <kirk.hardy@jellyfish.co>
2023-09-28 15:49:03 -0400

    || instead of exit code evaluation
    Co-authored-by: acicchetti-jf <106682337+acicchetti-jf@users.noreply.github.com>

--- rollback_on_fail.sh
+++ rollback_on_fail.sh
@@ -6,8 +6,4 @@ rollback () {
     python jf_agent-stable/jf_agent/main.py
 }
 
-timeout --preserve-status 10h python jf_agent/main.py
-EXITCODE=$?
-# timeout will kill 143, config exception sys.exit's with 1, 
-# exceptions throw various; only a completed run exits with 0
-if [[ $EXITCODE -ne 0 ]]; then rollback; fi
+timeout --preserve-status 10h python jf_agent/main.py || rollback
