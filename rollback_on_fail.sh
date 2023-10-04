e19b37f14d7b4eeb0468889a295e2e065ac1958b
Kirk Hardy <kirk.hardy@jellyfish.co>
2023-10-04 16:30:37 -0400

    echo statements

--- rollback_on_fail.sh
+++ rollback_on_fail.sh
@@ -5,7 +5,10 @@ rollback () {
     python jf_agent-stable/jf_agent/main.py
 }
 
-TOKEN=$(sed -n 's/JELLYFISH_API_TOKEN=//p' creds.env)
-TIME_LIMIT=$(curl -H "Accept: application/json" -H "Content-Type: application/json" -H "Jellyfish-API-Token: $TOKEN" https://app.jellyfish.co/endpoints/agent/time-limit | jq .time_limit_string)
+echo "Retrieving time limit value from JF"
+TIME_LIMIT=$(curl -H "Accept: application/json" -H "Content-Type: application/json" -H "Jellyfish-API-Token: $JELLYFISH_API_TOKEN" https://app.jellyfish.co/endpoints/agent/time-limit | jq .time_limit_string)
 TIME_LIMIT="${TIME_LIMIT:-12}"
+echo "checking for time limit override"
+TIME_LIMIT="${OVERRIDE_TIME_LIMIT:-$TIME_LIMIT}"
+echo "running agent with timelimit $TIME_LIMIT"
 timeout --preserve-status "$TIME_LIMIT" python jf_agent/main.py || echo "encountered error or timeout, rolling back to stable" && rollback
