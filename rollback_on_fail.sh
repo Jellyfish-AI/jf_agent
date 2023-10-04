6babfb6c239e3532c34da3d57e777ec4a0691e61
Kirk Hardy <kirk.hardy@jellyfish.co>
2023-10-04 15:23:42 -0400

    jq parse

--- rollback_on_fail.sh
+++ rollback_on_fail.sh
@@ -6,5 +6,6 @@ rollback () {
 }
 
 TOKEN=$(sed -n 's/JELLYFISH_API_TOKEN=//p' creds.env)
-TIME_LIMIT=$(curl -i -H "Accept: application/json" -H "Content-Type: application/json" -H "Jellyfish-API-Token: $TOKEN" https://app.jellyfish.co/endpoints/agent/time-limit) || 12
+TIME_LIMIT=$(curl -i -H "Accept: application/json" -H "Content-Type: application/json" -H "Jellyfish-API-Token: $TOKEN" https://app.jellyfish.co/endpoints/agent/time-limit | jq time_limit_string)
+TIME_LIMIT="${TIME_LIMIT:-12}"
 timeout --preserve-status "$TIME_LIMIT"h python jf_agent/main.py || rollback
