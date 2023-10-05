84ca9ad5259513ca15b63b6f45306313ab31c361
Kirk Hardy <kirk.hardy@jellyfish.co>
2023-10-05 08:42:41 -0400

    hour

--- rollback_on_fail.sh
+++ rollback_on_fail.sh
@@ -7,7 +7,7 @@ rollback () {
 
 echo "Retrieving time limit value from JF"
 TIME_LIMIT=$(curl -H "Accept: application/json" -H "Content-Type: application/json" -H "Jellyfish-API-Token: $JELLYFISH_API_TOKEN" https://app.jellyfish.co/endpoints/agent/time-limit | jq .time_limit_string)
-TIME_LIMIT="${TIME_LIMIT:-12}"
+TIME_LIMIT="${TIME_LIMIT:-12h}"
 echo "checking for time limit override"
 TIME_LIMIT="${OVERRIDE_TIME_LIMIT:-$TIME_LIMIT}"
 echo "running agent with timelimit $TIME_LIMIT"
