e5bd6d9d5fb03086d65f354fc11e9286ef0e4339
Kirk Hardy <kirk.hardy@jellyfish.co>
2023-10-05 08:57:55 -0400

    don't hit api if override present

--- rollback_on_fail.sh
+++ rollback_on_fail.sh
@@ -5,10 +5,16 @@ rollback () {
     python jf_agent-stable/jf_agent/main.py
 }
 
-echo "Retrieving time limit value from JF"
-TIME_LIMIT=$(curl -H "Accept: application/json" -H "Content-Type: application/json" -H "Jellyfish-API-Token: $JELLYFISH_API_TOKEN" https://app.jellyfish.co/endpoints/agent/time-limit | jq .time_limit_string)
-TIME_LIMIT="${TIME_LIMIT:-12h}"
+
 echo "checking for time limit override"
-TIME_LIMIT="${OVERRIDE_TIME_LIMIT:-$TIME_LIMIT}"
+if [[ -v OVERRIDE_TIME_LIMIT ]]; then
+	# May want to do some format validation here, I leave that to the reader
+	echo "Time limit override detected, agent will timeout at ${OVERRIDE_TIME_LIMIT}"
+	TIME_LIMIT="${OVERRIDE_TIME_LIMIT}"
+else
+	TIME_LIMIT=$(curl -H "Accept: application/json" -H "Content-Type: application/json" -H "Jellyfish-API-Token: $JELLYFISH_API_TOKEN" https://app.jellyfish.co/endpoints/agent/time-limit | jq .time_limit_string)
+	TIME_LIMIT="${TIME_LIMIT:-12h}"
+	echo "Time limit override not detected, retrieved timelimit from Jellyfish: (${TIME_LIMIT})"
+fi
 echo "running agent with timelimit $TIME_LIMIT"
 timeout --preserve-status "$TIME_LIMIT" python jf_agent/main.py || echo "encountered error or timeout, rolling back to stable" && rollback
