cab21a1c14914250cf55e034194fd05fac80caf1
Kirk Hardy <kirk.hardy@jellyfish.co>
2023-10-24 16:57:55 -0400

    logging level debug

--- jf_agent/jf_jira/__init__.py
+++ jf_agent/jf_jira/__init__.py
@@ -311,7 +311,7 @@ def load_and_dump_jira(config, endpoint_jira_info, jira_connection):
                 download_teams(jira_connection)
             )
         except Exception as e:
-            logging_helper.log_standard_error(logging.WARNING, msg_args=[e], error_code=0000, exc_info=True)
+            logging_helper.log_standard_error(logging.DEBUG, msg_args=[e], error_code=0000, exc_info=True)
 
         return {'type': 'Jira', 'status': 'success'}
 
