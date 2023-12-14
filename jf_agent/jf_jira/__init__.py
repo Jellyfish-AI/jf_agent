c940b3062bd25f6dc911cdfe5b7acdcd150281c1
John Ruelas <jruel4@yahoo.com>
2023-12-14 15:58:08 -0600

    Fix names

--- jf_agent/jf_jira/__init__.py
+++ jf_agent/jf_jira/__init__.py
@@ -214,12 +214,12 @@ def load_and_dump_jira(config, endpoint_jira_info, jira_connection):
         issue_ids_to_download = list(missing_issue_ids.union(out_of_date_issue_ids))
 
         for fname, vals in [
-            ('jira_issue_metadata_remote', issue_metadata_from_jira),
-            ('jira_issue_metadata_jellyfish', issue_metadata_from_jellyfish),
-            ('jira_issue_metadata_jellyfish_addl', issue_metadata_addl_from_jellyfish),
-            ('jira_issue_missing_issue_ids', missing_issue_ids),
-            ('jira_issue_out_of_date_issue_ids', out_of_date_issue_ids),
-            ('jira_issue_already_up_to_date_issue_ids', already_up_to_date_issue_ids),
+            ('dbg_jira_issue_metadata_remote', issue_metadata_from_jira),
+            ('dbg_jira_issue_metadata_jellyfish', issue_metadata_from_jellyfish),
+            ('dbg_jira_issue_metadata_jellyfish_addl', issue_metadata_addl_from_jellyfish),
+            ('dbg_jira_issue_missing_issue_ids', missing_issue_ids),
+            ('dbg_jira_issue_out_of_date_issue_ids', out_of_date_issue_ids),
+            ('dbg_jira_issue_already_up_to_date_issue_ids', already_up_to_date_issue_ids),
         ]:
             try:
                 write_file(config.outdir, fname, config.compress_output_files, vals)
