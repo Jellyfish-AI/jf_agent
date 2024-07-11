f278d47e651b6ad1c8956a8eddf982717f7320ac
John Ruelas <jruel4@yahoo.com>
2024-07-11 16:47:23 -0500

    misc logging not needed

--- rollback_on_fail.sh
+++ rollback_on_fail.sh
@@ -4,7 +4,7 @@ python jf_agent/main.py "$@"
 CODE=$?
 
 if [[ $CODE -ne 0 ]]; then
-    echo "encountered error or timeout"
+    echo "encountered error"
 
     echo "Will attempt to upload logs from the failed run, for debugging."
 
