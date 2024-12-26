b8f44082fff64541a025aa6a1d8db99aab496557
Eric L'Heureux <lheureuxe13@gmail.com>
2024-12-26 10:33:29 -0500

    [OJ-40908] Verbose Jira issue metadata logging (#379)
    * [OJ-40908] Verbose Jira issue metadata logging
    
    * use jf ingest retry for status
    
    * exc method
    
    * remove redundant logging

--- jf_agent/exception.py
+++ jf_agent/exception.py
@@ -1,2 +1,20 @@
+import logging
+import traceback
+from typing import Optional
+
+logger = logging.getLogger(__name__)
+
+
 class BadConfigException(Exception):
-    pass
+    def __init__(
+        self,
+        msg: str = 'BadConfigException (see earlier messages)',
+        exc: Optional[Exception] = None,
+    ):
+        super().__init__(msg)
+        logger.error(msg)
+
+        if exc:
+            exc_type = type(exc).__name__
+            logger.error(msg=f'{exc_type}: {str(exc)}')
+            logger.error(msg=f'Traceback:\n{traceback.format_exc()}')
