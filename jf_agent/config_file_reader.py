ddb9f958757b51c244ca83f3305ea49b51553361
Eric L'Heureux <lheureuxe13@gmail.com>
2025-07-29 14:15:03 -0400

    [OJ-45876] GHC Mannequin Users (#415)
    * [OJ-45876] GHC Mannequin Users
    
    * remove debug logging

--- jf_agent/config_file_reader.py
+++ jf_agent/config_file_reader.py
@@ -21,6 +21,8 @@ from jf_agent.util import get_company_info
 
 logger = logging.getLogger(__name__)
 
+GITHUB_DEFAULT_API_URL = 'https://api.github.com'
+
 
 @dataclass
 class GitConfig:
@@ -39,6 +41,7 @@ class GitConfig:
     gitlab_per_page_override: bool
     git_verbose: bool
     gitlab_keep_base_url: bool
+    github_check_mannequin_users: bool
     # For multi-git
     creds_envvar_prefix: str
     # legacy fields ==================
@@ -560,6 +563,7 @@ def get_ingest_config(
                 included_branches_by_repo=agent_git_config.git_include_branches,
                 git_redact_names_and_urls=agent_git_config.git_redact_names_and_urls,
                 git_strip_text_content=agent_git_config.git_strip_text_content,
+                check_ghc_mannequin_user_prs=agent_git_config.github_check_mannequin_users,
             )
         )
 
@@ -653,6 +657,12 @@ def _get_git_config(git_config, git_provider_override=None, multiple=False) -> G
         )
         raise BadConfigException()
 
+    # Mannequin users only exist in GitHub Cloud
+    pull_mannequin_user_prs = False
+    if git_provider == 'github':
+        if not isinstance(git_url, str) or GITHUB_DEFAULT_API_URL in git_url:
+            pull_mannequin_user_prs = True
+
     return GitConfig(
         git_provider=git_provider,
         git_instance_slug=git_instance_slug,
@@ -670,6 +680,7 @@ def _get_git_config(git_config, git_provider_override=None, multiple=False) -> G
         git_verbose=git_config.get('verbose', False),
         creds_envvar_prefix=creds_envvar_prefix,
         gitlab_keep_base_url=git_config.get('keep_base_url', False),
+        github_check_mannequin_users=pull_mannequin_user_prs,
         # legacy fields ===========
         git_include_bbcloud_projects=list(git_include_bbcloud_projects),
         git_exclude_bbcloud_projects=list(git_exclude_bbcloud_projects),
