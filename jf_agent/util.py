950170aa2c7d26d6266bc16b9e215cfc36866334
Ashley Redzko <ashley.redzko@jellyfish.co>
2025-08-04 17:41:54 -0400

    Test sonarqube webhooks

--- jf_agent/util.py
+++ jf_agent/util.py
@@ -42,6 +42,7 @@ def get_latest_agent_version():
 def split(lst: List[Any], n: int) -> List[List[Any]]:
     """
     Split list `lst` into `n` approximately equal chunks
+    Test line please delete later
     """
     k, m = divmod(len(lst), n)
     return (lst[i * k + min(i, m) : (i + 1) * k + min(i + 1, m)] for i in range(n))
