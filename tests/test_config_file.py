f76d1af5d675b0e57be5816e7c88ad3dbc6a9df9
Gavin Pitt <gavin@jellyfish.co>
2025-10-08 13:06:53 -0400

    OJ-48973: ensure typing of ado version

--- tests/test_config_file.py
+++ tests/test_config_file.py
@@ -136,3 +136,50 @@ class TestGitConfigGeneration(TestCase):
         assert auth_config.token == 'token-2'
         auth_config.api_version == '7.0'
         assert auth_config.verify is True
+
+    def test_get_jf_ingest_git_auth_config_convert_to_str(self):
+        ado_yaml_content = """
+        git:
+            - provider: ado
+              creds_envvar_prefix: ORG1
+              instance_slug: ado-instance-1
+              url: https://ado.com
+              verbose: true
+              ado_api_version: 7.1
+            - provider: ado
+              creds_envvar_prefix: ORG2
+              instance_slug: ado-instance-2
+              url: https://ado.com
+              verbose: true
+        """
+
+        yaml_config = yaml.safe_load(ado_yaml_content)
+
+        git_configs: list[GitConfig] = _get_git_config_from_yaml(yaml_config)
+        git_configs = sorted(git_configs, key=lambda x: x.git_instance_slug)
+
+        # Test first config
+        auth_config = _get_jf_ingest_git_auth_config(
+            company_slug='test-company',
+            config=git_configs[0],
+            git_creds={'ado_token': 'token-1'},
+            skip_ssl_verification=True,
+        )
+        assert type(auth_config) == JFIngestAzureDevopsAuthConfig
+        assert auth_config.company_slug == 'test-company'
+        assert auth_config.token == 'token-1'
+        assert auth_config.api_version == '7.1'
+        assert auth_config.verify is False
+
+        # Test second config
+        auth_config = _get_jf_ingest_git_auth_config(
+            company_slug='test-company',
+            config=git_configs[1],
+            git_creds={'ado_token': 'token-2'},
+            skip_ssl_verification=False,
+        )
+        assert type(auth_config) == JFIngestAzureDevopsAuthConfig
+        assert auth_config.company_slug == 'test-company'
+        assert auth_config.token == 'token-2'
+        assert auth_config.api_version == '7.0'
+        assert auth_config.verify is True
